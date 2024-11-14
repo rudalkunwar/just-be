402d2d6a015a6f3fc9f1198f685327235aacd494
rudalkunwar <ruzalkunwar@gmail.com>
2024-11-14 20:06:47 +0545

    card

--- src/components/practice/Card.jsx
+++ src/components/practice/Card.jsx
@@ -3,13 +3,11 @@ import React from 'react'
 function Card() {
     return (
         <div className='h-screen flex justify-center items-center bg-blue-50'>
-
             <div className='h-[80vh] mx-10 container bg-white shadow-lg'>
                 <div className='text-center'>
                     <div className='py-4'>
                         <h1 className='text-3xl'>Our Pricing</h1>
                     </div>
-
                     <div className='flex gap-5 justify-center'>
                         <h1>Annually</h1>
                         <input type="checkbox" />
@@ -17,7 +15,7 @@ function Card() {
                     </div>
                 </div>
                 <div className='flex py-12 mx-12 justify-center'>
-                    <div className='text-center h-[20rem] w-[20rem] shadow-lg'>
+                    <div className='text-center h-[20rem] w-[20rem] shadow-lg mt-10'>
                         <h2 className='py-2'>Basic</h2>
                         <div className='flex items-center justify-center font-bold py-4'>
                             <p className='py-2 text-2xl'>$</p>
@@ -27,7 +25,7 @@ function Card() {
                             <p className='border-b border-t py-2'>500 GB Storage</p>
                             <p className='border-b border-t py-2'>Best offer done</p>
                             <p className='border-b border-t py-2'>Buy this now</p>
-                            <button>Suscribe</button>
+                            <button className='bg-blue-600 text-white px-8 py-2 my-2 rounded-md'>Suscribe</button>
                         </div>
                     </div>
                     <div className='text-center text-white rounded-md  h-[25rem] w-[20rem] shadow-lg bg-violet-400'>
@@ -40,10 +38,10 @@ function Card() {
                             <p className='border-b border-t py-2'>500 GB Storage</p>
                             <p className='border-b border-t py-2'>Unlimited Acess</p>
                             <p className='border-b border-t py-2'>Lightining Fast Internet</p>
-                            <button>Suscribe</button>
+                            <button className='bg-blue-600 text-white px-8 py-2 my-2 rounded-md'>Suscribe</button>
                         </div>
                     </div>
-                    <div className='text-center h-[20rem] w-[20rem] shadow-lg'>
+                    <div className='text-center h-[20rem] w-[20rem] shadow-lg mt-10'>
                         <h2 className='py-2'>Master</h2>
                         <div className='flex items-center justify-center font-bold py-4'>
                             <p className='py-2 text-2xl'>$</p>
@@ -53,12 +51,11 @@ function Card() {
                             <p className='border-b border-t py-2'>250 GB Storage</p>
                             <p className='border-b border-t py-2'>Limited Acesss 10 req/sec</p>
                             <p className='border-b border-t py-2'>No die any today</p>
-                            <button>Suscribe</button>
+                            <button className='border border-black text-black px-8 py-2 my-2 rounded-md hover:bg-stone-200'>Suscribe</button>
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
     )
 }
