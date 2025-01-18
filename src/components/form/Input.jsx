cd1e35475d01469d1deb3d2434786113da0bebe4
rudalkunwar <ruzalkunwar@gmail.com>
2025-01-18 19:27:15 +0545

    reusable input component

--- src/components/form/Input.jsx
+++ src/components/form/Input.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import { Eye, EyeOff } from 'lucide-react'
+import React, { useState } from 'react'
 
 function Input({ label, type = 'text' }) {
+    const [inputType, setInputType] = useState(type);
     return (
         <div className='p-2 my-1 rounded-md'>
             <label htmlFor={label} className='block py-1'>{label}</label>
-            <input id={label} className='py-1 px-2 rounded-md border-2 border-indigo-400 outline-indigo-700' placeholder={`Enter ${label}`} type={type} />
-            
+            <div className='flex relative'>
+                <input id={label} className='py-1 px-2 rounded-md border-2 border-indigo-400 outline-indigo-700' placeholder={`Enter ${label}`} type={inputType} />
+                {
+                    type === 'password' && (
+                        inputType === 'text' ?
+                            (<EyeOff className='absolute right-1 top-1.5 z-10' onClick={() => setInputType((prev) => (prev === 'text' ? 'password' : 'text'))} />)
+                            :
+                            (< Eye className='absolute right-1 top-1.5 z-10' onClick={() => setInputType((prev) => (prev === 'text' ? 'password' : 'text'))} />)
+                    )
+                }
+            </div>
         </div>
     )
 }
